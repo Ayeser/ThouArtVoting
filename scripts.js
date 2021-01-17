42144b8ad59c236f603bbf47bb70c7e62ed4a479
Matthew Wester <MatthewWester@live.com>
2021-01-16 21:14:14 -0700

    Tailored type ahead feature to local json

--- scripts.js
+++ scripts.js
@@ -23,3 +23,41 @@ $('.panel').click(toggleOpen);
         this.classList.toggle('open-active');
       }
     }
+
+    const endpoint = 'https://raw.githubusercontent.com/Ayeser/ThouArtVoting/main/bestOfBook.json';
+
+    const books = [];
+    fetch(endpoint)
+      .then(blob => blob.json())
+      .then(data => books.push(...data));
+    
+    function findMatches(wordToMatch, books) {
+      return books.filter(selection => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return selection.book.match(regex)
+      });
+    }
+    
+    // function numberWithCommas(x) {
+    //   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    // }
+    
+    function displayMatches() {
+      const matchArray = findMatches(this.value, books);
+      const html = matchArray.map(selection => {
+        const regex = new RegExp(this.value, 'gi');
+        const bookName = selection.book.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+          <li>
+            <span class="name">${bookName}</span>
+          </li>
+        `;
+      }).join('');
+      suggestions.innerHTML = html;
+    }
+    
+    const searchInput = document.querySelector('.search');
+    const suggestions = document.querySelector('.suggestions');
+    
+    searchInput.addEventListener('change', displayMatches);
+    searchInput.addEventListener('keyup', displayMatches);
